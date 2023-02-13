030ad16cde6fea0783da83ccea61cbce8846650d
nitzanpap <nitzanpap@gmail.com>
2023-02-13 02:46:07 +0200

    Created basic intro screen functionality

--- lib/screens/introduction_screen.dart
+++ lib/screens/introduction_screen.dart
@@ -1,6 +1,6 @@
 import 'package:dispatcher/global_constants.dart';
-import 'package:dispatcher/widgets/progress_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class IntroductionScreen extends StatefulWidget {
   const IntroductionScreen({super.key});
@@ -10,11 +10,19 @@ class IntroductionScreen extends StatefulWidget {
 }
 
 class _IntroductionScreenState extends State<IntroductionScreen> {
-  List intro_descriptions = [
+  final List introDescriptions = [
     'Welcome to Dispatcher, the right way to read your news. Just open the app.',
     'Search your fields of interest and the best part..',
     'Save all your articles for later, filter, learn and explore the latest news.',
   ];
+  int currentStep = 0;
+
+  void moveToNextStep() {
+    setState(() {
+      if (currentStep + 1 <= introDescriptions.length - 1) currentStep++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,15 +33,46 @@ class _IntroductionScreenState extends State<IntroductionScreen> {
         alignment: Alignment.topCenter,
         child: Column(
           children: [
-            ProgressBar(
-              totalSteps: 3,
+            SizedBox(
+              width: 260,
+              child: Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: LinearProgressIndicator(
+                      color: Theme.of(context).colorScheme.error,
+                      value: (currentStep + 1) / introDescriptions.length,
+                      backgroundColor: Colors.grey[800],
+                      minHeight: 10,
+                    ),
+                  ),
+                  const Gap(40),
+                  Text(
+                    appTitle,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 32,
+                    ),
+                  ),
+                  const Gap(40),
+                  Text(
+                    introDescriptions[currentStep],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              appTitle,
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.onPrimary,
-                fontWeight: FontWeight.bold,
-                fontSize: 32,
+            ElevatedButton(
+              onPressed: () => moveToNextStep(),
+              child: Row(
+                children: [
+                  Text('Next'),
+                ],
               ),
             ),
           ],
